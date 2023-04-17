036c1cfb10635397e5656a247dd2f50c80efd784
wyatthacks420 <122548054+wyatthacks420@users.noreply.github.com>
2023-04-17 10:13:30 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let sonar = 0
 basic.showIcon(IconNames.Chessboard)
 wuKong.setAllMotor(100, 100)
 basic.forever(function () {
-    sonar = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P0)
+    sonar = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
     if (sonar < 20 && sonar > 1) {
         wuKong.setAllMotor(50, -100)
         basic.pause(randint(0, 10))
